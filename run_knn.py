8baa9955b498d5c93a3dfdb05c1a92a435d9688d
Chungsang Tom Lam <erinata@gmail.com>
2019-03-12 17:55:47 -0400

    use knn to predict

--- run_knn.py
+++ run_knn.py
@@ -11,7 +11,35 @@ data = iris.iloc[:,2:6]
 
 print(data.head())
 
-target = iris.iloc[:,1]
+target = iris.iloc[:,1].values
 
 print(target)
+
+knn = KNeighborsClassifier(n_neighbors=6)
+
+knn.fit(data, target)
+
+X = [
+    [4.9, 1.1, 1.1, 1.2],
+    [3.5, 2.0, 1.3, 4.8],
+    [6.0, 3.0, 2.4, 2.0],
+    [6.8, 3.2, 5.9, 2.3],
+]
+
+print(X)
+
+results = knn.predict(X)
+
+print(results)
+
+
+
+
+
+
+
+
+
+
+
 
